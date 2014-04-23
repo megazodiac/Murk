853ab02e8b9bf0fdde1e1c7704c3bc592ee51a28
megazodiac <tcl299@nyu.edu>
2014-04-22 22:29:39 -0400

    Create ReplaceReturn.h

---
+++ ReplaceReturn.h
@@ -0,0 +1,11 @@
+//
+//  ReplaceReturn.h
+//  Murk
+//
+//  Created by Tyler Law on 4/22/14.
+//  Copyright (c) 2014 Tyler Law. All rights reserved.
+//
+
+
+//Function is just an fgets modifier
+void ReplaceReturn(char *mystring);
