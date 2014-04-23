350ed48aec0b176b91fc730baa64ffd93eac7103
megazodiac <tcl299@nyu.edu>
2014-04-23 00:01:00 -0400

    Update Globals.h

--- Globals.h
+++ Globals.h
@@ -1,10 +1,14 @@
 //
-//  ReplaceReturn.h
+//  Globals.h
 //  Murk
 //
 //  Created by Tyler Law on 4/22/14.
 //  Copyright (c) 2014 Tyler Law. All rights reserved.
 //
 
-//This function is just an fgets modifier
-void ReplaceReturn(char *mystring);
+//Array is for 'Press enter to continue' effect
+char space[3];
+char playername[30];
+
+// Just a loop variable
+int j = 0;
