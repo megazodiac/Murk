1113bd8060e387302c4afca3df8262ebb51d24a3
megazodiac <tcl299@nyu.edu>
2014-04-23 00:00:37 -0400

    Create Globals.h

---
+++ Globals.h
@@ -0,0 +1,10 @@
+//
+//  ReplaceReturn.h
+//  Murk
+//
+//  Created by Tyler Law on 4/22/14.
+//  Copyright (c) 2014 Tyler Law. All rights reserved.
+//
+
+//This function is just an fgets modifier
+void ReplaceReturn(char *mystring);
